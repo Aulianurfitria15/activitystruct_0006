203c0278841c6170f2d4f353c423a0c824c1ee6b
Aulianurfitria15 <aulianurfitria17@gmail.com>
2024-04-02 18:25:05 +0700

    menambahkan variabel global contoh 2

--- struct2/struct2.cpp
+++ struct2/struct2.cpp
@@ -1,30 +1,4 @@
 #include <iostream>
 using namespace std;
 
-struct DetailAlamat {
-	string desa;
-	string kota;
-};
 
-struct mahasiswa {
-	string nim;
-	string nama;
-	DetailAlamat alamat;
-	int umur;
-};
-
-int main() {
-	mahasiswa mhs;
-	cout << "Nomor mahasiswa : ";
-	getline(cin, mhs.nim);
-	cout << "Nama Mahasiswa : ";
-	getline(cin, mhs.nama);
-	cout << "alamat mahasiswa : " << endl;
-	cout << "\t nama Desa : ";
-	cin >> mhs.alamat.desa;
-	cout << "\t Nama kota : ";
-	cin >> mhs.alamat.kota;
-	cout << "Umur mahasiswa : ";
-	cin >> mhs.umur;
-
-	
