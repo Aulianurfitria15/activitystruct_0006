525c429715519c1dbc5424797381c5f94efa1312
Aulianurfitria15 <aulianurfitria17@gmail.com>
2024-04-02 18:17:47 +0700

    getline alamat mahasiswa

--- struct2/struct2.cpp
+++ struct2/struct2.cpp
@@ -19,3 +19,8 @@ int main() {
 	getline(cin, mhs.nim);
 	cout << "Nama Mahasiswa : ";
 	getline(cin, mhs.nama);
+	cout << "alamat mahasiswa : " << endl;
+	cout << "\t nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t Nama kota : ";
+	cin >> mhs.alamat.kota;
